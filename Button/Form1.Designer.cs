ff60d3635eb8f91509c797098a5e1e74f84f16b6
Rose <miniusbhater@gmail.com>
2025-04-25 19:21:19 +0100

    Add files via upload

---
+++ Button/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Button
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new System.Windows.Forms.Button();
+            label1 = new Label();
+            button2 = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(12, 79);
+            button1.Name = "button1";
+            button1.Size = new Size(152, 117);
+            button1.TabIndex = 0;
+            button1.Text = "YT";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(270, 47);
+            label1.TabIndex = 1;
+            label1.Text = "YouTube button";
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(170, 79);
+            button2.Name = "button2";
+            button2.Size = new Size(121, 117);
+            button2.TabIndex = 2;
+            button2.Text = "i like youtube";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(303, 222);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
